59b3dcc64efd0cc879e6dd1e37c7426ce06b68cc
pawel.minkina@icsolutions.pl <pawel.minkina@icsolutions.pl>
2020-12-22 12:21:03 +0100

    Add project files.

---
+++ PrinterDriverApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinterDriverApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            testydrivera testydrivera = new testydrivera();
+            Console.WriteLine("The end");
+            Console.ReadKey();
+
+        }
+    }
+}
